409815df80f6b651dbb1d5c296761c9ed153f784
Shady GOMAA <shadyahmed9719@gmail.com>
2022-12-22 09:53:10 +0200

    create simple example

---
+++ run.bat
@@ -0,0 +1,9 @@
+#to configure robot correctly follow the steps
+#step1: install python 
+#step2: check python installation using python --version && pip --version
+#step3: install robot using pip install robotframework && robotframework-seleniumlibrary
+#step4: set environment path to python , lib , lib/site-packages/robot
+#step5: install selenium support server and intelbot plugins in intelj
+#step6: add robot file with siple testcase and open terminal in intelj
+#step7: run this command robot test/testcase.robot
+#to update pip python.exe -m pip install --upgrade pip
